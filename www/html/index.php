7b722a263e7e984f72aaa02f9c6785007423683d
fukuba-shuma <calculus.fukuba@gmail.com>
2022-02-15 00:00:29 +0900

    最大点数取得関数実装

--- www/html/index.php
+++ www/html/index.php
@@ -24,6 +24,7 @@ choices: <?php var_dump(getChoices($db, 1)) ?>
 <form action="" method="POST">
     <input type="hidden" name="token" value="<?= htmlspecialchars($_SESSION['token'], ENT_QUOTES) ?>">
 <?php foreach($questions as $question): ?>
+    <input type="hidden" name="ids[]" value="<?= $question['id'] ?>">
     <h1><?= $question['title'] ?></h1>
     <?php $choices = getChoices($db, $question['id']) ?>
     <?php foreach($choices as $choice): ?>
