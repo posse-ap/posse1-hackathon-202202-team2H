c443ed93940cb4b0338426a62ba3e74410b81759
sanukiudon27 <uudodo0207@icloud.com>
2022-02-16 08:05:21 +0900

    jquery実装

--- www/html/scriptresult.js
+++ www/html/scriptresult.js
@@ -39,3 +39,23 @@ $('#result').on('inview', function (event, isInView) {
         });
     }
 });
+
+
+// posse紹介が下からスクロール
+$(window).on('load scroll', function (){
+
+    var box = $('.fadeIn');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 10 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
