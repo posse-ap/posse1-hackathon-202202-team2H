cb638fa76a3010f0d1c02237c2acf65ee9e50578
fukuba-shuma <calculus.fukuba@gmail.com>
2022-02-16 06:54:24 +0900

    topページのスタイルを調整

--- www/html/scriptindex.js
+++ www/html/scriptindex.js
@@ -4,7 +4,7 @@ document.addEventListener('scroll', () => {
     for (var i = 0; i < scrollers.length; i++) {
         const elementDistance = scrollers[i].getBoundingClientRect().top + scrollers[i].clientHeight * .6;
         const qutestionTitle = scrollers[i].querySelector('.question_stmt');
-        const questionChoices = scrollers[i].querySelector('ul');
+        const questionChoices = scrollers[i].querySelector('.question_choices');
 
         if (i === 1) {
             console.log(elementDistance);
@@ -14,6 +14,5 @@ document.addEventListener('scroll', () => {
             questionChoices.classList.add('show');
             // scrollers[i].classList.add('show');
         }
-
     }
 });
