cdf427ffe0fe87abfe2d5882f8198d0debb144af
fukuba-shuma <calculus.fukuba@gmail.com>
2022-02-15 14:43:51 +0900

    header

--- www/html/header.php
+++ www/html/header.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>posse診断ページ</title>
+  <link rel="stylesheet" href="../html/css/reset.css">
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <link rel="stylesheet" href="../html/css/style.css">
 </head>
+
 <body>
-    <header>
-    </header>
+  <!-- header -->
+  <header class="header">
+    <div class='header_container'>
+      <a href=""><img src="../../www/html/img/posseLogo.png" alt=""></a>
+      <p>大学生向けプログラミング学習コミュニティ</p>
+    </div>
+  </header>
+  <!-- end pf header -->
